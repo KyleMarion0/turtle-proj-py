237aae265e88f1d81b6e3cbc4d7bb08c401b8d9c
Kyle Marion <73936202+KyleMarion0@users.noreply.github.com>
2023-01-11 02:54:02 -0500

    Update main.py
    Used rgb values for random colors instead of a list

--- main.py
+++ main.py
@@ -2,16 +2,23 @@ from turtle import Turtle, Screen
 import random
 
 tim = Turtle()
+screen = Screen()
+screen.colormode(255)
 
-colors = ["CornflowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue"]
+def random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    rgb_values = (r, g, b)
+    return rgb_values
+
 direction = [0, 90, 180, 270]
 
 tim.pensize(15)
 tim.speed(10)
-for _ in range(100):
-    tim.color(random.choice(colors))
+for _ in range(250):
+    tim.color(random_color())
     tim.forward(25)
     tim.setheading(random.choice(direction))
 
-screen = Screen()
 screen.exitonclick()
